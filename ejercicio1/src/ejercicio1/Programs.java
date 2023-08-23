6255d315cad2bf5b6801f9074c6ed5e274ad162d
frntml <134443996+frntml@users.noreply.github.com>
2023-08-23 09:52:44 -0300

    Add files via upload

---
+++ ejercicio1/src/ejercicio1/Programs.java
@@ -0,0 +1,12 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Programs {
+	public static void main(String[] args) {
+		var sc = new Scanner (System.in);
+		System.out.print("ingrese un nombre: ");
+		var nombre= sc.nextLine();
+		System.out.print("el nombre que ingreso es: " + nombre);
+	}
+}
